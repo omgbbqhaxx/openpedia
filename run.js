5416451341d6ffdf6b29a05588e627c6d418fa20
Yasin Aktimur <chardone@windowslive.com>
2019-01-15 06:05:31 +0300

    Venezule için ispanyolca eklendi.

--- run.js
+++ run.js
@@ -3,9 +3,15 @@ var i;
 for (i = 0; i < x.length; i++) { 
     var myurl = x[i].href
     //console.log(myurl);
-    var n = myurl.search("https://wikipedia.org");
-    var j = myurl.search("https://tr.wikipedia.org");
-    if(n != -1 || j != -1) {
+    var orj = myurl.search("https://wikipedia.org");
+    var turk = myurl.search("https://tr.wikipedia.org");
+    var esp = myurl.search("https://es.wikipedia.org");
+    var fr = myurl.search("https://fr.wikipedia.org");
+    var eng = myurl.search("https://en.wikipedia.org");
+    var az = myurl.search("https://az.wikipedia.org");
+
+    
+    if(orj != -1 || turk != -1 || esp != -1|| fr != -1|| eng != -1|| az != -1) {
         console.log(myurl);
         newurl = myurl.split("wikipedia");
         var fullurl = newurl[0]+"0wikipedia"+newurl[1]
